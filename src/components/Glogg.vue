20e920f8a588417eb4297586782e74ea049a0b9e
Axel Ekwall <axel@minimize.se>
2017-12-01 19:44:12 +0100

    Ad D3.js

--- src/components/Glogg.vue
+++ src/components/Glogg.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="glogg">
-      <h1>Glögg!!</h1>
+      <h1>Vilken glögg är bäst? OL har hela listan!</h1>
+      <p>Ol har testat all glögg och här är resultatet.</p>
       <div id="charts"></div>
   </div>
 </template>
 
 <script>
+const data = [
+  {
+    name: 'Glögg 1',
+    score: 5
+  },
+  {
+    name: 'Glögg 2',
+    score: 3
+  }
+];
+
 export default {
   name: 'Glogg',
   data() {
     return {
-      results: [
-        {
-          name: 'Glögg 1',
-          score: 5
-        },
-        {
-          name: 'Glögg 2',
-          score: 3
-        }
-      ]
+      results: data
     };
   }
 };
