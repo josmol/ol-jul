999588139acf831eaafc0ff094962fe30834df8e
josmol <josmol@kth.se>
2017-12-01 19:36:44 +0100

    backgrounds and buttons

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -12,12 +11,22 @@ export default {
 </script>
 
 <style>
+
+body{
+    width: 100%;
+    height: 100%;
+    background-image: url("./assets/julbakgrund.png");
+    background-position: center center;
+    background-repeat:  no-repeat;
+    background-attachment: fixed;
+    background-size:  cover;   
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: white;
   margin-top: 60px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,12 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <button v-on:click="julrim">JULRIM</button>
+    <audio id="rim-audio">
+      <source type="audio/wav" src="../assets/ho-ho.wav"></audio>
+    </audio>
+    <button v-on:click="julklappar">JULKLAPPSTIPS</button>
+    <button v-on:click="glogg">OL TESTAR GLÖGG</button>
   </div>
 </template>
 
@@ -25,7 +15,14 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'JUL ÄR KUL'
+    }
+  },
+  methods: {
+    julrim: function () {
+      console.log("CLICKED JULRIM BUTTON")
+      var audio = document.getElementById("rim-audio");
+      audio.play();
     }
   }
 }
@@ -46,5 +43,25 @@ li {
 }
 a {
   color: #42b983;
+}
+
+button{
+   background-image: url("../assets/sign.png");
+  background-position: center center;
+  background-repeat:  no-repeat;
+  background-color: transparent;
+  background-size:contain;
+  height: 50px;
+  width: 200px;
+  outline: none;
+  border: none;
+  font-size: 18px;
+  color:#331a00;
+  font-weight: bold;
+}
+
+button:hover{
+  height: 60px;
+  width: 160px;
 }
 </style>
