0406ad626b3e131ab3c829812302ed20cf565b95
Axel Ekwall <axel@minimize.se>
2017-12-01 17:56:15 +0100

    Add glögg component and sass-loader

---
+++ src/components/Glogg.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="glogg">
+      <h1>Glögg!!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Glogg',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Glogg from '@/components/Glogg'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/glogg',
+      name: 'Glogg',
+      component: Glogg
     }
   ]
 })
