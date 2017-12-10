23989f7b890174ca867eeb18876c32ad92c8dc91
Axel Ekwall <axelekwall@users.noreply.github.com>
2017-12-10 16:39:09 +0100

    Updates from Axels branch (#2)
    * environment commit
    
    * Moved everyting out of the ol-jul folder
    
    * Updated gitignore
    
    * Npm install + gitignore
    
    * Add glögg component and sass-loader
    
    * Update code format
    
    * backgrounds and buttons
    
    * Ad D3.js
    
    * no warnings
    
    * sounds, buttons and little modal
    
    * SANTA LFYING
    
    * SNÖ!
    
    * Refactor and move particles to its own component
    
    * Renamed HelloWorld and updated routes
    
    * Started working with glogg component and charts
    
    * Merge from master - fix
    
    * Small fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,28 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '@/components/Home';
+import Glogg from '@/components/Glogg';
+import Chart1 from '@/components/Charts/Chart1';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/glogg',
+      component: Glogg,
+      children: [
+        {
+          path: '',
+          name: 'glogg',
+          component: Chart1
+        }
+      ]
     }
   ]
-})
+});
