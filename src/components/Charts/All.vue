19298f62435795cacb5e7a689eefbe1243dac113
Axel Ekwall <axel@minimize.se>
2017-12-16 23:40:46 +0100

    Started working on charts

---
+++ src/components/Charts/All.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="chart-all">
+    <svg class="chart-all"></svg>
+  </div>
+</template>
+
+<script>
+import * as d3 from 'd3';
+import results from '@/assets/gloggResults';
+export default {
+  name: 'Chart-all',
+  data() {
+    return {
+      width: 500,
+      height: 500,
+      results: results
+    };
+  },
+  mounted() {
+    d3
+      .select('svg.chart-all')
+      .attr('width', this.width)
+      .attr('height', this.height);
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/Charts/Single.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="chart-single">
+    <p>Chart-Single {{nr}} </p>
+    <svg class="chart-single-svg"></svg>
+  </div>
+</template>
+
+<script>
+import results from '@/assets/gloggResults';
+export default {
+  name: 'Chart-single',
+  props: ['nr'],
+  data() {
+    return {
+      results: results
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.chart-single {
+  height: 500px;
+  margin: 30px;
+}
+</style>
