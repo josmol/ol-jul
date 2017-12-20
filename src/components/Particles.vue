480dfd114f4b798e34bf44383fe78e170cb45d75
josmol <josmol@kth.se>
2017-12-20 20:42:51 +0100

    snow - lite mindre upplösning
    tror jag

--- src/components/Particles.vue
+++ src/components/Particles.vue
@@ -4,110 +4,116 @@
 
 <script>
 /* eslint-disable */
+
 const particlesSettings = {
-  particles: {
-    number: {
-      value: 400,
-      density: {
-        enable: true,
-        value_area: 800
+  "particles": {
+    "number": {
+      "value": 284,
+      "density": {
+        "enable": false,
+        "value_area": 801.7060304327614
       }
     },
-    color: {
-      value: '#fff'
+    "color": {
+      "value": "#fff"
     },
-    shape: {
-      type: 'circle',
-      stroke: {
-        width: 0,
-        color: '#000000'
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
       },
-      polygon: {
-        nb_sides: 5
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
       }
     },
-    opacity: {
-      value: 0.5,
-      random: true,
-      anim: {
-        enable: false,
-        speed: 1,
-        opacity_min: 0.1,
-        sync: false
+    "opacity": {
+      "value": 0.43292125643369117,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
       }
     },
-    size: {
-      value: 3.945738208161363,
-      random: true,
-      anim: {
-        enable: false,
-        speed: 40,
-        size_min: 0.1,
-        sync: false
+    "size": {
+      "value": 5,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 38.97970619046457,
+        "size_min": 0.1,
+        "sync": false
       }
     },
-    line_linked: {
-      enable: false,
-      distance: 500,
-      color: '#ffffff',
-      opacity: 0.4,
-      width: 2
+    "line_linked": {
+      "enable": false,
+      "distance": 500,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 2
     },
-    move: {
-      enable: true,
-      speed: 1.3,
-      direction: 'bottom',
-      random: false,
-      straight: false,
-      out_mode: 'out',
-      bounce: false,
-      attract: {
-        enable: false,
-        rotateX: 600,
-        rotateY: 1200
+    "move": {
+      "enable": true,
+      "speed": 3,
+      "direction": "bottom",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
       }
     }
   },
-  interactivity: {
-    detect_on: 'canvas',
-    events: {
-      onhover: {
-        enable: true,
-        mode: 'bubble'
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false,
+        "mode": "bubble"
       },
-      onclick: {
-        enable: true,
-        mode: 'push'
+      "onclick": {
+        "enable": true,
+        "mode": "push"
       },
-      resize: true
+      "resize": true
     },
-    modes: {
-      grab: {
-        distance: 400,
-        line_linked: {
-          opacity: 0.5
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 0.5
         }
       },
-      bubble: {
-        distance: 170,
-        size: 4,
-        duration: 0.3,
-        opacity: 0.5,
-        speed: 3
+      "bubble": {
+        "distance": 400,
+        "size": 4,
+        "duration": 0.3,
+        "opacity": 1,
+        "speed": 3
       },
-      repulse: {
-        distance: 200,
-        duration: 0.4
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
       },
-      push: {
-        particles_nb: 4
+      "push": {
+        "particles_nb": 4
       },
-      remove: {
-        particles_nb: 2
+      "remove": {
+        "particles_nb": 2
       }
     }
   },
-  retina_detect: true
+  "retina_detect": false
 };
 
 export default {
