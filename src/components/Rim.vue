9f27ac70c938f8870bf566be4047f3a7cf24d6c9
Frida <friekl@kth.se>
2017-12-20 14:00:40 +0100

    RIM

--- src/components/Rim.vue
+++ src/components/Rim.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="rim">
     <div class="rim-content">
-      <h1>JULRIM</h1>
-      <button v-on:click="back">Tillbaka</button>
+      <p id="whattogive">Vad ska du ge bort?</p>
+
+      <div class="selectgift">
+      <button v-on:click="showstrumpor" class="nicebutton">Strumpor</button>
+      <button v-on:click="showbok" class="nicebutton">En bok</button>
+      <button v-on:click="showparadis" class="nicebutton">Paradisask</button>
+      </div>
+      <div class="selectgift">
+        <button v-on:click="showcd" class="nicebutton">CD/Vinyl</button>
+        <button v-on:click="showjacka" class="nicebutton">Jacka</button>
+        <button v-on:click="showspegel" class="nicebutton">Spegel</button>
+      </div>
+      <div id="displayrim"><p>{{rim}}</p></div>
+
+      <button v-on:click="back">Tillbaka</button>        
     </div>
   </div>
 </template>
@@ -10,11 +23,34 @@
 <script>
 export default {
   name: 'Rim',
+  data() {
+    return {
+      rim: ""
+    };
+  },
   methods: {
     back: function () {
       this.$router.push({name: 'home'});
+    },
+    showstrumpor: function(){
+      this.rim = "Har du varit snäll i år? Då slipper du frysa om dina tår."
+    },
+    showbok: function(){
+      this.rim = "Bladen på denna är inte gröna, de har samma färg som en vit höna."
+    },
+    showparadis: function(){
+      this.rim = "Don't think outside the box, här är en present som är väldigt ortodox"
+    },
+    showjacka: function(){
+      this.rim = "Jag orkar inte snacka, här har du en jacka"
+    },
+    showcd: function(){
+      this.rim = "Rund med hål i, hoppas den inte är dåli’"
+    },
+    showspegel: function(){
+      this.rim = "I denna kan du titta när du vill ögonbrynen hitta"
     }
-  }
+  } 
 };
 </script>
 
@@ -26,19 +62,24 @@ export default {
   align-items: center;
   width: 100vw;
   height: 100vh;
+  background-color:rgba(0,0,0,0.4)
 }
 
 .rim-content {
   display: flex;
   align-items: center;
   flex-direction: column;
-  background: white;
-  border-radius: 10px;
+  background-color: #100a30;
+  background-image: url('../assets/rhyme_background.jpg');
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  margin: 0;  border-radius: 10px;
   width: 70vw;
-  min-height: 50vh;
+  min-height: 60vh;
 }
 
-button {
+button, .nicebutton {
   background-image: url('../assets/sign.png');
   background-position: center center;
   background-repeat: no-repeat;
@@ -55,8 +96,64 @@ button {
   margin-bottom: 30px;
 }
 
-button:hover {
-  height: 55px;
-  width: 205px;
+.nicebutton{
+  height: 35px;
+  width: 110px;
+  font-size:15px;
+  margin:10px 10px;
 }
+
+#whattogive{
+  font-size: 18px;
+  color:white;
+  font-weight: bold;
+  margin-top:8vh;
+}
+
+
+.selectgift{
+  display:block;
+}
+
+p{
+  color:black;
+}
+
+.gift{
+  display:inline-block;
+  margin:0px 20px;
+}
+
+#displayrim{
+  background-image: url('../assets/christmascard.jpg');
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-color: transparent;
+  width:40vw;
+  height:10vw;
+  margin-top:30px;
+  margin-bottom:30px;
+  margin-left:10%;
+  margin-right:10%;
+  padding-top:2vh;
+  padding-left:1.5vh;
+  padding-right:1.5vh;
+  font-size:20px;
+  color:black;
+}
+
+#displayrim p{
+  margin-right:5vw;
+  margin-left:5vw;
+}
+
+@media screen and (max-width: 760px) {
+    #displayrim{
+      width:50vw;
+      height:33vw;
+      padding-top:0px;
+    }
+}
+
+
 </style>
